39848a17031784079c487188677120365771a94e
Ankur9669 <65125998+Ankur9669@users.noreply.github.com>
2021-06-29 17:44:49 +0530

    added click functionality on icons

--- whats-app-clone/src/components/SidebarLeftHeader.js
+++ whats-app-clone/src/components/SidebarLeftHeader.js
@@ -6,8 +6,15 @@ import MoreVertIcon from '@material-ui/icons/MoreVert';
 import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
 import "../css/sidebarleftsearchbar.css";
+import {useState} from "react";
+function SidebarLeftHeader() 
+{
+    const [isOptionContainerOpen, setOptionContainerVisibility] = useState(false);
+    function onClickMoreIcon()
+    {
+        setOptionContainerVisibility(!isOptionContainerOpen);
+    }
 
-function SidebarLeftHeader() {
     return (
         <div className = "sidebar-left-header-container">
         <div className = "sidebar-left-header">
@@ -28,9 +35,20 @@ function SidebarLeftHeader() {
                     <MessageIcon className = "icons"/>
                 </IconButton>
 
-                <IconButton>
+                <IconButton onClick = {onClickMoreIcon}>
                     <MoreVertIcon className = "icons"/>
                 </IconButton>
+                {isOptionContainerOpen && 
+                    <div className = "options-container">
+                        <ul className = "options">
+                            <li className = "option">New Group</li>
+                            <li className = "option">Profile</li>
+                            <li className = "option">Settings</li>
+                            <li className = "option">Log out</li>
+                        </ul>
+                    </div>
+                }
+                
             </div>
         </div>
         <div className = "sidebar-left-search-bar-container">
